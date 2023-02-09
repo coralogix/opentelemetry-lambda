ae71345d9883388ec6008c11d8f3b4b2f5130113
Povilas Versockas <povilas.versockas@coralogix.com>
2023-02-09 20:39:40 +0200

    feat: add python-autoinstrumentation

---
+++ python/sample-apps/layer.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+set -e
+set -u
+
+echo_usage() {
+	echo "usage: Deploy Lambda layer/application by SAM"
+	echo " -r <aws region>"
+	echo " -t <cloudformation template>"
+	echo " -b <sam build>"
+	echo " -d <deploy>"
+	echo " -n <specify layer name>"
+	echo " -l <show layer arn>"
+	echo " -s <stack name>"
+}
+
+is_sample() {
+	if [[ $(pwd) == *"sample"* ]]; then
+		echo 1
+	else
+		echo 0
+	fi
+}
+
+main() {
+	echo "running..."
+	saved_args="$@"
+	template='template.yml'
+	build=false
+	deploy=false
+	layer=false
+
+	region=${AWS_REGION}
+	stack=${OTEL_LAMBDA_STACK-"otel-stack"}
+	layerName=${OTEL_LAMBDA_LAYER-"otel-layer"}
+
+
+	collectorPath=${COLLECTOR_PATH-"../../collector"}
+
+	while getopts "hbdxlr:t:s:n:" opt; do
+		case "${opt}" in
+		h)
+			echo_usage
+			exit 0
+			;;
+		b)
+			build=true
+			;;
+		d)
+			deploy=true
+			;;
+		n)
+			layerName="${OPTARG}"
+			;;
+		l)
+			layer=true
+			;;
+		r)
+			region="${OPTARG}"
+			;;
+		t)
+			template="${OPTARG}"
+			;;
+		s)
+			stack="${OPTARG}"
+			;;
+		\?)
+			echo "Invalid option: -${OPTARG}" >&2
+			exit 1
+			;;
+		:)
+			echo "Option -${OPTARG} requires an argument" >&2
+			exit 1
+			;;
+		esac
+	done
+
+	echo "Invoked with: ${saved_args}"
+
+	if [[ $build == false && $deploy == false && $layer == false ]]; then
+		build=true
+		deploy=true
+		layer=true
+	fi
+
+	if [[ $build == true ]]; then
+		echo "sam building..."
+
+		echo "run.sh: building the collector..."
+		pushd $collectorPath || exit
+		make package
+		rm build/collector-extension.zip
+		popd || exit
+		#rm -rf otel/collector_build/
+		##cp -r $collectorPath/build/ otel/collector_build/
+
+		echo "run.sh: Starting sam build."
+		sam build -u -t $template
+	fi
+
+	if [[ $deploy == true ]]; then
+		sam deploy --stack-name $stack --region $region --capabilities CAPABILITY_NAMED_IAM --resolve-s3 --parameter-overrides LayerName=$layerName
+		rm -rf otel/otel_collector
+		rm -rf .aws-sam
+	fi
+
+	if [[ $layer == true ]]; then
+		echo -e "\nOTel Lambda layer ARN:"
+		arn=$(aws lambda list-layer-versions --layer-name $layerName --region $region --query 'max_by(LayerVersions, &Version).LayerVersionArn')
+		echo $arn | sed 's/\"//g'
+	fi
+}
+
+main "$@"
