e9de87a33588ab079fe68f6781ca065f939d2425
Rafał Sumisławski <rafal.sumislawski@coralogix.com>
2023-08-10 15:49:46 +0200

    fix shellcheck errors

--- ci-scripts/publish_layer.sh
+++ ci-scripts/publish_layer.sh
@@ -2,16 +2,16 @@
 
 set -euo pipefail
 
-for region in ${REGIONS[@]}; do
+for region in "${REGIONS[@]}"; do
     if [ "$LIMITED_REGION" = true ] ; then
-        output=`aws lambda publish-layer-version --layer-name ${LAYER_NAME}${NAME_SUFFIX} --compatible-runtimes $COMPATIBLE_RUNTIMES --zip-file fileb://target/layer.zip --region $region`
+        output=$(aws lambda publish-layer-version --layer-name "${LAYER_NAME}${NAME_SUFFIX}" --compatible-runtimes "$COMPATIBLE_RUNTIMES" --zip-file fileb://target/layer.zip --region "$region")
     else 
-        output=`aws lambda publish-layer-version --layer-name ${LAYER_NAME}${NAME_SUFFIX} --compatible-architectures x86_64 arm64 --compatible-runtimes $COMPATIBLE_RUNTIMES --zip-file fileb://target/layer.zip --region $region`
+        output=$(aws lambda publish-layer-version --layer-name "${LAYER_NAME}${NAME_SUFFIX}" --compatible-architectures x86_64 arm64 --compatible-runtimes "$COMPATIBLE_RUNTIMES" --zip-file fileb://target/layer.zip --region "$region")
     fi
-    version=`echo "$output" | jq -r .Version` 
-    versionArn=`echo "$output" | jq -r .LayerVersionArn`
+    version=$(echo "$output" | jq -r .Version)
+    versionArn=$(echo "$output" | jq -r .LayerVersionArn)
     if [ "$PUBLIC" = true ] ; then
-        aws lambda add-layer-version-permission --layer-name ${LAYER_NAME}${NAME_SUFFIX} --principal '*' --action lambda:GetLayerVersion --version-number $version --statement-id public --region $region > /dev/null
+        aws lambda add-layer-version-permission --layer-name "${LAYER_NAME}${NAME_SUFFIX}" --principal '*' --action lambda:GetLayerVersion --version-number "$version" --statement-id public --region "$region" > /dev/null
     fi
-    echo $versionArn
+    echo "$versionArn"
 done
