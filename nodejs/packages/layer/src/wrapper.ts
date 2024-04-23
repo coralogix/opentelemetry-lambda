91646867ba72a90e22f7e88b9d3172f6bb25e7ff
Rafał Sumisławski <rafal.sumislawski@coralogix.com>
2024-04-23 10:08:57 +0200

    Enable module loading hook if handler is in an mjs file

--- nodejs/packages/layer/src/wrapper.ts
+++ nodejs/packages/layer/src/wrapper.ts
@@ -1,7 +1,3 @@
-import { register } from "node:module"; 
-import { pathToFileURL } from "node:url"; 
-register("@opentelemetry/instrumentation/hook.mjs", pathToFileURL("./"));
-
 import {
   NodeTracerConfig,
   NodeTracerProvider,
