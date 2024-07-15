00eb5b7b530cd8c67cb82572ec8f66edd5ab1347
Rafał Sumisławski <rafal.sumislawski@coralogix.com>
2024-07-15 15:58:42 +0200

    Extract python building to a bash script

---
+++ ci-scripts/build_python_layer.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ -z "${OPENTELEMETRY_PYTHON_CONTRIB_PATH:-}" ]; then
+    echo "OPENTELEMETRY_PYTHON_CONTRIB_PATH is not set"
+    exit 1
+fi
+OPENTELEMETRY_PYTHON_CONTRIB_PATH=$(realpath $OPENTELEMETRY_PYTHON_CONTRIB_PATH)
+
+CWD=$(pwd)
+
+echo OPENTELEMETRY_PYTHON_CONTRIB_PATH=$OPENTELEMETRY_PYTHON_CONTRIB_PATH
+echo CWD=$CWD
+
+# Build build opentelemetry-instrumentation-aws-lambda
+pushd ./python/sample-apps/otel
+pip3 wheel -e $OPENTELEMETRY_PYTHON_CONTRIB_PATH/instrumentation/opentelemetry-instrumentation-aws-lambda
+pip3 wheel -e $OPENTELEMETRY_PYTHON_CONTRIB_PATH/instrumentation/opentelemetry-instrumentation-botocore
+mkdir -p ./build
+python3 -m pip install -r ./otel_sdk/requirements.txt -t ./build/python
+python3 -m pip install -r ./otel_sdk/requirements-nodeps.txt -t ./build/tmp --no-deps
+cp -r ./build/tmp/* ./build/python/
+rm -rf ./build/tmp
+cp ./otel_sdk/otel-instrument ./build/otel-instrument
+chmod 755 ./build/otel-instrument
+cp ./otel_sdk/otel-instrument ./build/otel-handler
+chmod 755 ./build/otel-handler
+cp ./otel_sdk/otel_wrapper.py ./build/python/
+rm -rf ./build/python/boto*
+rm -rf ./build/python/urllib3*
+popd > /dev/null
+
+pushd ./python/sample-apps/otel/build
+zip -r layer.zip *
+popd > /dev/null
