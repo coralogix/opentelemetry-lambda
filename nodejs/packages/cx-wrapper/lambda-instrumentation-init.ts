0c2ba2c4d364aa1f763513fcd89bea31ee4f441d
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2025-04-11 15:18:46 +0200

    [nodejs/cx-wrapper] Add per-trigger context extractor

--- nodejs/packages/cx-wrapper/lambda-instrumentation-init.ts
+++ nodejs/packages/cx-wrapper/lambda-instrumentation-init.ts
@@ -2,16 +2,64 @@ import {
   defaultTextMapGetter,
   Context as OtelContext,
   propagation,
-  trace,
   diag,
-  ROOT_CONTEXT
+  ROOT_CONTEXT,
+  trace,
 } from '@opentelemetry/api';
+import type { Context } from 'aws-lambda';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { AwsLambdaInstrumentation, AwsLambdaInstrumentationConfig } from '@opentelemetry/instrumentation-aws-lambda';
+import { findLambdaTrigger } from '@opentelemetry/instrumentation-aws-lambda';
 import { OTEL_PAYLOAD_SIZE_LIMIT, OtelAttributes } from './common.js';
 
 declare global {
   function configureLambdaInstrumentation(config: AwsLambdaInstrumentationConfig): AwsLambdaInstrumentationConfig
+}
+
+function headerContextExtractor(event: any): OtelContext | undefined {
+  if (!event?.headers) {
+    return undefined;
+  }
+  return propagation.extract(ROOT_CONTEXT, event.headers, defaultTextMapGetter);
+}
+
+function clientContextExtractor(_event: any, context: Context): OtelContext | undefined {
+  if (context.clientContext?.Custom) {
+    try {
+      const extractedClientContextOtelContext: OtelContext =
+        propagation.extract(
+          ROOT_CONTEXT,
+          context.clientContext.Custom,
+          defaultTextMapGetter
+        );
+      if (trace.getSpan(extractedClientContextOtelContext)?.spanContext()) {
+        return extractedClientContextOtelContext;
+      }
+    } catch (e) {
+      diag.debug(
+        'error extracting context from lambda client context payload',
+        e
+      );
+    }
+  } else if ((context.clientContext as any)?.custom) {
+    try {
+      const extractedClientContextOtelContext: OtelContext =
+        propagation.extract(
+          ROOT_CONTEXT,
+          (context.clientContext as any).custom,
+          defaultTextMapGetter
+        );
+      if (trace.getSpan(extractedClientContextOtelContext)?.spanContext()) {
+        return extractedClientContextOtelContext;
+      }
+    } catch (e) {
+      diag.debug(
+        'error extracting context from lambda client context payload',
+        e
+      );
+    }
+  }
+  return undefined;
 }
 
 export function makeLambdaInstrumentation(): AwsLambdaInstrumentation {
@@ -31,55 +79,13 @@ export function makeLambdaInstrumentation(): AwsLambdaInstrumentation {
       }
     },
     disableAwsContextPropagation: true,
-    eventContextExtractor: (event, context) => {
-      // try to extract propagation from http headers first
-      const httpHeaders = event?.headers || {};
-      const extractedHttpContext: OtelContext = propagation.extract(
-        ROOT_CONTEXT,
-        httpHeaders,
-        defaultTextMapGetter
-      );
-      if (trace.getSpan(extractedHttpContext)?.spanContext()) {
-        return extractedHttpContext;
-      }
-
-      // extract from client context
-      if (context.clientContext?.Custom) {
-        try {
-          const extractedClientContextOtelContext: OtelContext =
-            propagation.extract(
-              ROOT_CONTEXT,
-              context.clientContext.Custom,
-              defaultTextMapGetter
-            );
-          if (trace.getSpan(extractedClientContextOtelContext)?.spanContext()) {
-            return extractedClientContextOtelContext;
-          }
-        } catch (e) {
-          diag.debug(
-            'error extracting context from lambda client context payload',
-            e
-          );
-        }
-      } else if ((context.clientContext as any)?.custom) {
-        try {
-          const extractedClientContextOtelContext: OtelContext =
-            propagation.extract(
-              ROOT_CONTEXT,
-              (context.clientContext as any).custom,
-              defaultTextMapGetter
-            );
-          if (trace.getSpan(extractedClientContextOtelContext)?.spanContext()) {
-            return extractedClientContextOtelContext;
-          }
-        } catch (e) {
-          diag.debug(
-            'error extracting context from lambda client context payload',
-            e
-          );
-        }
-      }
-      return ROOT_CONTEXT;
+    eventContextExtractor: (event: any, context: Context): OtelContext => {
+      const headerContext = headerContextExtractor(event);
+      const clientContext = clientContextExtractor(event, context);
+      const triggerContext = findLambdaTrigger(event)?.contextExtractor?.(event);
+      const extractedContext = [headerContext, clientContext, triggerContext]
+        .find((context: OtelContext | undefined) => context !== undefined) ?? ROOT_CONTEXT;
+      return extractedContext;
     },
     payloadSizeLimit: OTEL_PAYLOAD_SIZE_LIMIT,
   };
@@ -87,7 +93,7 @@ export function makeLambdaInstrumentation(): AwsLambdaInstrumentation {
   // TODO consider not treating it as an instrumentation
   const instrumentation = new AwsLambdaInstrumentation(typeof configureLambdaInstrumentation === 'function' ? configureLambdaInstrumentation(lambdaAutoInstrumentConfig) : lambdaAutoInstrumentConfig)
 
-  registerInstrumentations({instrumentations: [instrumentation]})
+  registerInstrumentations({ instrumentations: [instrumentation] })
 
   return instrumentation
 }
